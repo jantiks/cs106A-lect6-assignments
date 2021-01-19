48b84f646c723a0606b96e101fcbbdc064e92e9d
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-19 16:05:06 +0400

    checkboard assignment is ready

--- src/problem1.java
+++ src/problem1.java
@@ -1,5 +1,24 @@
+/*
+this class draws checkboard
+ */
+
 import acm.program.*;
 import acm.graphics.*;
 
-public class problem1 {
+public class problem1 extends GraphicsProgram {
+    public void run() {
+        int NROWS = 8;
+        int NCOLS = 8;
+        int sqrSize = getHeight() / NROWS ;
+
+        for (int i = 0; i < NROWS; i++) {
+            for (int j = 0; j < NCOLS; j++) {
+                int x = i * sqrSize;
+                int y = j * sqrSize;
+                GRect rect = new GRect(x,y,sqrSize,sqrSize);
+                rect.setFilled((i + j) % 2 != 0);
+                add(rect);
+            }
+        }
+    }
 }
